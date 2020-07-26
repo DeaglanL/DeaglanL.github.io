d0e44d4bee239904ad23ae7bbabbb8c4d66cafe0
Deaglan Lynch <DeaglanLynch@hotmail.co.uk>
2020-07-26 22:08:53 +0100

    Began building home page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import CVData from './Data/CVData'
+import Header from './Component/Header';
+import About from './Component/About';
+import Footer from './Component/Footer';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Header />
+        <About data = {this.state.CVData.About}/>
+        <Footer />
     </div>
   );
 }
